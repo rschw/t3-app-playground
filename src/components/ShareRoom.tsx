e26f134751c87f990e0bd0e4a92c4086835f6de6
trebor <hagen.robert@protonmail.com>
2022-07-24 14:51:59 +0200

    cleanup landing page

--- src/components/ShareRoom.tsx
+++ src/components/ShareRoom.tsx
@@ -1,6 +1,6 @@
 import { QRCodeSVG } from "qrcode.react";
 import toast from "react-hot-toast";
-import { FaShareAlt } from "react-icons/fa";
+import { FaCopy } from "react-icons/fa";
 
 const copyUrlToClipboard = (url: string) => {
   navigator.clipboard.writeText(url);
@@ -11,13 +11,14 @@ const ShareRoom: React.FC<{ roomId: string }> = ({ roomId }) => {
   const shareUrl = `${window.location.origin}/room/${roomId}`;
 
   return (
-    <section className="flex flex-col-reverse items-center gap-4 md:flex-row md:justify-between md:items-start">
+    <section className="flex flex-col-reverse items-center gap-6 md:flex-row md:justify-between md:items-start">
       <button
-        className="text-violet-500 flex justify-center gap-4 px-4 py-2 rounded shadow shadow-violet-300"
+        className="flex justify-center gap-4 px-4 py-2 rounded shadow shadow-violet-300"
         onClick={() => copyUrlToClipboard(shareUrl)}
       >
-        Room {roomId}
-        <FaShareAlt size={24} />
+        <span className="text-violet-500">Room</span>
+        <span className="font-light">{roomId}</span>
+        <FaCopy size={24} className="text-violet-500" />
       </button>
       <QRCodeSVG value={shareUrl} />
     </section>
